5c89110b7caa00d2bc2eaf717d025f45252abdd5
Alejandro Martinez Ruiz <alex@flawedcode.org>
2017-02-20 15:55:24 +0000

    reactor: time tracking annotations

--- lib/puma/reactor.rb
+++ lib/puma/reactor.rb
@@ -17,6 +17,8 @@ module Puma
       @timeouts = []
 
       @sockets = [@ready]
+      @t = TimeTracker.new :idle
+      @t.start
     end
 
     private
@@ -25,8 +27,9 @@ module Puma
       sockets = @sockets
 
       while true
+        @t.to :run_internal
         begin
-          ready = IO.select sockets, nil, nil, @sleep_for
+          ready = @t.into(:select) { IO.select sockets, nil, nil, @sleep_for }
         rescue IOError => e
           if sockets.any? { |socket| socket.closed? }
             STDERR.puts "Error in select: #{e.message} (#{e.class})"
@@ -41,6 +44,7 @@ module Puma
         if ready and reads = ready[0]
           reads.each do |c|
             if c == @ready
+              @t.into(:pipe_command) do
               @mutex.synchronize do
                 case @ready.read(1)
                 when "*"
@@ -59,20 +63,26 @@ module Puma
                   return
                 end
               end
+	      end
             else
               # We have to be sure to remove it from the timeout
               # list or we'll accidentally close the socket when
               # it's in use!
               if c.timeout_at
+		@t.into(:timeout_delete) do
                 @mutex.synchronize do
                   @timeouts.delete c
                 end
+		end
               end
 
+	      @t.to :client_io
               begin
-                if c.try_to_finish
+                if c.into(:try_to_finish) { c.try_to_finish }
+                  @t.into(:client_to_pool) do
                   @app_pool << c
                   sockets.delete c
+		  end
                 end
 
               # SSL handshake failure
@@ -81,6 +91,7 @@ module Puma
                 addr = ssl_socket.peeraddr.last
                 cert = ssl_socket.peercert
 
+		c.to :sslerror
                 c.close
                 sockets.delete c
 
@@ -88,6 +99,7 @@ module Puma
 
               # The client doesn't know HTTP well
               rescue HttpParserError => e
+                c.to :parser_error
                 c.write_400
                 c.close
 
@@ -95,6 +107,7 @@ module Puma
 
                 @events.parse_error @server, c.env, e
               rescue StandardError => e
+                c.to :standard_error
                 c.write_500
                 c.close
 
@@ -104,20 +117,24 @@ module Puma
           end
         end
 
+	@t.to :timeout_management
         unless @timeouts.empty?
           @mutex.synchronize do
+            @t.into(:timeout_discard) do
             now = Time.now
 
             while @timeouts.first.timeout_at < now
               c = @timeouts.shift
+	      c.to :timeouted
               c.write_408 if c.in_data_phase
               c.close
               sockets.delete c
 
               break if @timeouts.empty?
             end
+	    end
 
-            calculate_sleep
+	    @t.into(:calculate_sleep) { calculate_sleep }
           end
         end
       end
@@ -137,10 +154,12 @@ module Puma
         begin
           run_internal
         rescue StandardError => e
+          @t.to :standard_error
           STDERR.puts "Error in reactor loop escaped: #{e.message} (#{e.class})"
           STDERR.puts e.backtrace
           retry
         ensure
+          @t.to :closing
           @trigger.close
           @ready.close
         end
@@ -162,16 +181,21 @@ module Puma
     end
 
     def add(c)
+      @t.into(:client_add) do
       @mutex.synchronize do
+        c.to :reactor
         @input << c
         @trigger << "*"
 
         if c.timeout_at
-          @timeouts << c
-          @timeouts.sort! { |a,b| a.timeout_at <=> b.timeout_at }
+          c.into(:reactor_sort) do
+            @timeouts << c
+            @timeouts.sort! { |a,b| a.timeout_at <=> b.timeout_at }
 
-          calculate_sleep
+            calculate_sleep
+          end
         end
+      end
       end
     end
 
